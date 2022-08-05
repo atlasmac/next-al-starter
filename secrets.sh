fc58f0a81b5b9b28698baae90a946b148773eac8
atlasmac <mckinleyatlas@gmail.com>
2022-08-04 18:43:37 -0600

    secrets change

--- secrets.sh
+++ secrets.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 rm secrets.json
 echo { >> secrets.json
-echo "\"CALENDAR_PRIVATE_KEY\": \""$CALENDAR_PRIVATE_KEY"\"" >> secrets.json
-echo "\"CALENDAR_CLIENT_EMAIL\": \""$CALENDAR_CLIENT_EMAIL"\"" >> secrets.json
+echo "\"CALENDAR_PRIVATE_KEY\": \""$CALENDAR_PRIVATE_KEY"\"," >> secrets.json
+echo "\"CALENDAR_CLIENT_EMAIL\": \""$CALENDAR_CLIENT_EMAIL"\"," >> secrets.json
 echo "\"CALENDAR_ID\": \""$CALENDAR_ID"\"" >> secrets.json
 echo } >> secrets.json
