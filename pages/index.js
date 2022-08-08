8a4881e96d2d9954c4195f30287de1a0736cb4e3
atlasmac <mckinleyatlas@gmail.com>
2022-08-08 12:30:23 -0600

    Add secrets for sheets

--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,6 @@ import Spotify from '../components/Spotify';
 import { fetchEvents } from '../utils/events';
 
 export async function getServerSideProps() {
-  console.log(JSON.stringify(process.env, null, 2))
-  console.error(JSON.stringify(process.env, null, 2))
   const events = await fetchEvents()
   // const articles = await fetchArticles()
   return {
--- secrets.sh
+++ secrets.sh
@@ -3,5 +3,8 @@ rm secrets.json
 echo { >> secrets.json
 echo "\"CALENDAR_PRIVATE_KEY\": \""$CALENDAR_PRIVATE_KEY"\"," >> secrets.json
 echo "\"CALENDAR_CLIENT_EMAIL\": \""$CALENDAR_CLIENT_EMAIL"\"," >> secrets.json
-echo "\"CALENDAR_ID\": \""$CALENDAR_ID"\"" >> secrets.json
+echo "\"CALENDAR_ID\": \""$CALENDAR_ID"\"," >> secrets.json
+echo "\"SHEETS_PRIVATE_KEY\": \""$SHEETS_PRIVATE_KEY"\"," >> secrets.json
+echo "\"SHEETS_CLIENT_EMAIL\": \""$SHEETS_CLIENT_EMAIL"\"," >> secrets.json
+echo "\"SHEETS_ID\": \""$SHEETS_ID"\"" >> secrets.json
 echo } >> secrets.json
