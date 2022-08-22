d674b31daa675c786503d13e93ba6c3e4ed1bdd0
atlasmac <mckinleyatlas@gmail.com>
2022-08-21 21:52:25 -0600

    remove contanct form

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
 import '../styles/globals.css';
-import { FormspreeProvider } from '@formspree/react';
+// import { FormspreeProvider } from '@formspree/react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
-import ContactForm from '../components/ContactForm';
+// import ContactForm from '../components/ContactForm';
 import {useEffect, useState } from 'react';
 import Head from 'next/head';
 
@@ -19,9 +19,9 @@ function MyApp({ Component, pageProps }) {
         showHeaderLogo={showHeaderLogo}
         setShowHeaderLogo={setShowHeaderLogo}
       />
-      <FormspreeProvider project="1995716952659590692">
+      {/* <FormspreeProvider project="1995716952659590692">
         <ContactForm />
-      </FormspreeProvider>
+      </FormspreeProvider> */}
       <Footer />
       {/* <script async src="https://cdn.snipcart.com/themes/v3.2.2/default/snipcart.js" /> */}
       {/* <div hidden id="snipcart" data-api-key="YzI2ZGMwYjQtOTM4NS00MWJhLWIyMmMtY2E2NzhjOWY3ZjEzNjM3OTQ5NzgzMDA2MzU4NTUy" style={{ zIndex: 10000000000 }} /> */}
