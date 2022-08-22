236bd26bd392b278d0f781e5cdc1986cc515ce81
atlasmac <mckinleyatlas@gmail.com>
2022-08-21 21:41:37 -0600

    remove snipcart links

--- pages/shop.js
+++ pages/shop.js
@@ -16,9 +16,9 @@ const Shop = ({ setShowHeaderLogo, showHeaderLogo }) => {
         <title>Al&apos;s Shop</title>
         <meta name="The shop for Al Maul Music" content="Al Maul's shop" />
         <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://app.snipcart.com" />
+        {/* <link rel="preconnect" href="https://app.snipcart.com" />
         <link rel="preconnect" href="https://cdn.snipcart.com" />
-        <link rel="stylesheet" href="https://cdn.snipcart.com/themes/v3.2.2/default/snipcart.css" />
+        <link rel="stylesheet" href="https://cdn.snipcart.com/themes/v3.2.2/default/snipcart.css" /> */}
       </Head>
       {/* <ShopComponent
         products={products}
