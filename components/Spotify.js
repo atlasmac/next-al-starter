96d190dcfbe6379d670526a50de126b1f73c9910
atlasmac <mckinleyatlas@gmail.com>
2022-08-21 21:36:14 -0600

    test spotify

--- components/Spotify.js
+++ components/Spotify.js
@@ -9,10 +9,16 @@ const Spotify = () => {
           <h2><a href="#" className={styles.coolLink}>Listen on Spotify</a></h2>
         </div>
         <p>Al Mauls latest release <i>The Wandering</i>.</p>
-        <iframe className={styles.spotify} htmlstyle="border-radius:5px"
-          src="https://open.spotify.com/embed/album/0RwmVLqSpavFhZ93SsSAZn?utm_source=generator" width="100%"
-          height="380" frameBorder="" htmlallowfullscreen=""
-          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+        <iframe style={{borderRadius: '12px'}}
+          src="https://open.spotify.com/embed/album/0RwmVLqSpavFhZ93SsSAZn?utm_source=generator"
+          width="100%"
+          height="380"
+          frameBorder="0"
+          allowFullScreen=""
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+          >
+
+        </iframe>
       </div>
     </div>
   )
